d0f4da07298fc239587f51c532e22423822160ae
cecil <cecilghimire@gmail.com>
2023-01-11 09:33:10 +0545

    usermodel

--- UsermodelImplemantation/UsermodelImplemantation/settings.py
+++ UsermodelImplemantation/UsermodelImplemantation/settings.py
@@ -27,10 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -116,3 +116,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL='userapp.User'
