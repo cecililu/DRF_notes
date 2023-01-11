a7954729a3cb54394fcc96c0bf7b8186101e0484
cecil <cecilghimire@gmail.com>
2023-01-11 09:59:49 +0545

    aded teacher and student models

--- UsermodelImplemantation/userapp/models.py
+++ UsermodelImplemantation/userapp/models.py
@@ -1,21 +1,48 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 
 class User(AbstractUser):
     class Role(models.TextChoices):
         ADMIN="ADMIN",'Admin'
         STUDENT="STUDENT",'Student'
         TEACHER="TEACHER",'Teacher'
-    #for specific case where by defaut admin
+    #for specific case where by defaut admin all user to be admin
     base_role=Role.ADMIN
-    
-    role=models.CharField('Role',max_length=50,choices=Role.choices)
+     
+    role=models.CharField(max_length=50,choices=Role.choices)
     
     def save(self,*args,**kwargs):
-        if not self.pk():
+        if not self.pk:
             self.role=self.base_role
             return super().save(*args,**kwargs)
     
+class StudentManager(BaseUserManager):
+    def get_queryset(self,*args,**kwargs):
+        results=super().get_queryset(*args,**kwargs)
+        return results.filter(role=User.Role.STUDENT)
+
+class Student(User):
+    base_role=User.Role.STUDENT
+    student=StudentManager()
+    
+    class Meta:
+        proxy=True
         
+    def welcome(self):
+        return "Only for students"
+    
+class TeacherManager(BaseUserManager):
+    def get_queryset(self,*args,**kwargs):
+        results=super().get_queryset(*args,**kwargs)
+        return results.filter(role=User.Role.TEACHER)
+
+class Teacher(User):
+    base_role=User.Role.STUDENT
+    student=TeacherManager()
+    
+    class Meta:
+        proxy=True
         
-        
+    def welcome(self):
+        return "Only for Teacher"
+    
