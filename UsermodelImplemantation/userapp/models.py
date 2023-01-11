d915194f00d4b2d0691ede962434a7d3ab877869
cecil <cecilghimire@gmail.com>
2023-01-11 12:48:15 +0545

    added teacher and student profile using signals

--- UsermodelImplemantation/userapp/models.py
+++ UsermodelImplemantation/userapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser,BaseUserManager
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class User(AbstractUser):
     class Role(models.TextChoices):
@@ -30,6 +32,16 @@ class Student(User):
         
     def welcome(self):
         return "Only for students"
+  
+@receiver(post_save,sender=Student)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created and instance.role=="STUDENT":
+        StudentProfile.objects.create(user=instance)
+          
+class StudentProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    student_id=models.IntegerField(null=True,blank=True)
+    
     
 class TeacherManager(BaseUserManager):
     def get_queryset(self,*args,**kwargs):
@@ -37,12 +49,21 @@ class TeacherManager(BaseUserManager):
         return results.filter(role=User.Role.TEACHER)
 
 class Teacher(User):
-    base_role=User.Role.STUDENT
-    student=TeacherManager()
+    base_role=User.Role.TEACHER
+    teacher=TeacherManager()
     
     class Meta:
         proxy=True
         
     def welcome(self):
         return "Only for Teacher"
+    
+@receiver(post_save,sender=Teacher)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created and instance.role=="TEACHER":
+        TeacherProfile.objects.create(user=instance)
+          
+class TeacherProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    teacher_id=models.IntegerField(null=True,blank=True)
     
