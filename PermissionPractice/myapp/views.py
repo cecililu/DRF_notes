c719c13753cd4afcb369b9274ea26bb1b467c4d8
cecil <cecilghimire@gmail.com>
2023-01-15 10:43:00 +0545

    aded permissions to the List view

--- PermissionPractice/myapp/views.py
+++ PermissionPractice/myapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import ListView
 
-# Create your views here.
+from myapp.models import Post
+
+class PostListView(PermissionRequiredMixin, ListView):
+    
+    permission_required = "myapp.view_post"
+    template_name = "post.html"
+    model = Post
