43350a547a59df7389e45880616d2a58f5247822
cecil <cecilghimire@gmail.com>
2023-01-16 14:12:00 +0545

    aded model

---
+++ customrestpermissions/DisasterApi/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Municipality(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+   
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import AbstractUser
+
+class Main(models.Model):
+    name=models.CharField(max_length=200)
+    muni=models.ForeignKey(Municipality,on_delete=models.CASCADE)
+    
+class User(AbstractUser,PermissionsMixin):
+    muni = models.ForeignKey(Municipality,blank=True,on_delete=models.CASCADE)
+    
